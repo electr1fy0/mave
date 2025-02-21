5bfaa0921b5bf52e10b01b639eb43372374840d5
electr1fy0 <electr1fy0@icloud.com>
2025-02-21 19:29:24 +0530

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
 # Intro
-
 Building my own text editor following [antirez's guide](https://viewsourcecode.org/snaptoken/kilo/).
+
+# References
+[unistd.h Man page](https://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html)
+[termios Man page](https://linux.die.net/man/3/termios)
+
+# Progress
+- [x] Setup
+- [x] Entering raw mode
+- [ ] Raw input and output
+- [ ] A text viewer
+- [ ] A text editor
+- [ ] Search
+- [ ] Syntax highlighting
+
+
+# Notes
+** will format them later **
+int tcgetattr(int fd, struct termios *termios_p);
+
+Retrieves the terminal configuration and stores it in a struct termios.
+
+Parameters:
+ • fd → File descriptor of the terminal (usually STDIN_FILENO, which is 0).
+ • termios_p → Pointer to a struct termios that stores the settings.
+
+Return Value:
+ • 0 → Success
+ • -1 → Error (sets errno)
+
+errno
+
+A global variable in C that stores an error code when a system call or library function fails.
+
+Standard File Descriptors
+
+Macro Value Description
+STDIN_FILENO 0 Standard input
+STDOUT_FILENO 1 Standard output
+STDERR_FILENO 2 Standard error
+
+perror
+
+Prints an error message describing the last errno value.
+
+perror("Error message");
--- mave.c
+++ mave.c
@@ -7,6 +7,28 @@
 #include <termios.h>
 #include <unistd.h>
 
+
+
+// NOTES //
+// int tcgetattr(int fd, struct termios *termios_p);
+// This retrieves the terminal config and stores it in a struct of
+// type termios.
+// fd stands for file descriptor of the terminal (usually STDIN_FILENO
+// which is 0)
+// termios_p is a pointer to a struct termios
+// return value:
+// 0 is success -1 sets errno
+//
+// errno is a global var in C that stores error code when a system call or library fn
+// fails
+// STDIN_FILENO (macro)
+// 0 for stdin
+// 1 for stdout
+// 2 for std error
+//
+// perror is description of errno, print error funny syntax tbh
+
+
 /// DATA ///
 struct termios orig_termios;
 
@@ -18,27 +40,30 @@ void die(const char *s) {
 
 void disableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+      // “Terminal Control Set Attributes - Flush” // set after flush
     die("tcsetattr");
 }
 
 void enableRawMode() {
-
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
     die("tcgetattr");
   atexit(disableRawMode);
-  struct termios raw = orig_termios;
+  struct termios raw = orig_termios; // copy orig into a new variable to be modified
 
+
+  //
   raw.c_iflag &= (~BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_iflag &= ~(IXON | ICRNL);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 
-  raw.c_cc[VMIN] = 0;
-  raw.c_cc[VTIME] = 1;
+  raw.c_cc[VMIN] = 0; // min byte count needed before read() can return
+  raw.c_cc[VTIME] = 1; // max time to wait before read() returns
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
     die("tcsetattr");
 }
+
 
 /// INIT ///
 int main() {
